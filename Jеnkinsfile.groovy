28de6cc6740c4d51bc356ebe427d9dd49d570628
Hristo Ivanov <hristo.ivanov@upnetix.com>
2019-09-09 11:24:21 +0300

    Jenkinsfile push command inserted

--- Jеnkinsfile.groovy
+++ Jеnkinsfile.groovy
@@ -5,11 +5,10 @@ pipeline {
       steps {
         bat 'mvn package'
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'
+        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git add . https://HristoVenelinovIvanov:Prodcho@github.com/HristoVenelinovIvanov/Project.git.biz/file.git'
+        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git commit -m "Push from Jenkinsfile" https://HristoVenelinovIvanov:Prodcho@github.com/HristoVenelinovIvanov/Project.git.biz/file.git'
+        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git push https://HristoVenelinovIvanov:Prodcho@github.com/HristoVenelinovIvanov/Project.git.biz/file.git'
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'
-        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git add .'
-        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git commit -m "Push from Jenkinsfile"'
-        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git push'
-        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status' 
       }
     }
   }

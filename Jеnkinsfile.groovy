99e0963fcd09e1860b557c226f4953ca2f5d3b4b
Hristo Ivanov <hristo.ivanov@upnetix.com>
2019-09-09 15:15:54 +0300

    Refactoring Jenkinsfile

--- Jеnkinsfile.groovy
+++ Jеnkinsfile.groovy
@@ -4,9 +4,9 @@ pipeline {
     stage('build') {
       steps {
         bat 'mvn package'
-        bat 'git config core.autocrlf true'
-        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git add .'
+        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git config core.autocrlf true'
+        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'
         bat 'git commit -m "Push from Jenkinsfile"'
         bat 'git push'
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'

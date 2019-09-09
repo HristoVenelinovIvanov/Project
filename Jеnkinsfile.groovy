af4dc1be32165d2944c3427014588e6f5ebd1348
unknown <UXLT027$@upnetix.com>
2019-09-09 13:52:37 +0300

    Push from Jenkinsfile

--- Jеnkinsfile.groovy
+++ Jеnkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline {
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git add .'
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git commit -m "Push from Jenkinsfile"'
-        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git push'
+        bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git push' 
         bat 'cd C:\\Users\\hristo.ivanov\\TechnomarketProject\\Project && git status'
       }
     }
